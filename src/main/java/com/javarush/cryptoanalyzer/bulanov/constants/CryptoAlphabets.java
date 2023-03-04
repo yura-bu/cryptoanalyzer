454cdd430bef9af2856c833e3dfe851b5cb2b55e
yura-bu <yura.bulanov@gmail.com>
2023-03-04 11:14:11 +0300

    CryptoAlphabets.java added

---
+++ src/main/java/com/javarush/cryptoanalyzer/bulanov/constants/CryptoAlphabets.java
@@ -0,0 +1,9 @@
+package com.javarush.cryptoanalyzer.bulanov.constants;
+
+public class CryptoAlphabets{
+    private static final String LETTERS_UPPER_CASE = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String LETTERS_LOWER_CASE = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    private static final String NUMBERS = "0123456789";
+    private static final String SYMBOLS = ".,\":-!? ";
+    public static final String  ALPHABET = LETTERS_UPPER_CASE + LETTERS_LOWER_CASE + NUMBERS + SYMBOLS;
+}
