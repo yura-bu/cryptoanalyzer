000df48e07ab47d2e7d6fb27d4ec2d5007d97616
yura-bu <yura.bulanov@gmail.com>
2023-03-05 13:44:17 +0300

    FileCrypto.java and FileRead.java changed

--- src/main/java/com/javarush/cryptoanalyzer/bulanov/repository/FileRead.java
+++ src/main/java/com/javarush/cryptoanalyzer/bulanov/repository/FileRead.java
@@ -1,13 +1,17 @@
 package com.javarush.cryptoanalyzer.bulanov.repository;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class FileRead{
 
     public StringBuilder fileRead(){
         StringBuilder text = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+        String pathFile = scanner.nextLine();
+        scanner.close();
 
-        try(FileInputStream fileInputStream = new FileInputStream("ExampleText.txt");
+        try(FileInputStream fileInputStream = new FileInputStream(pathFile);
            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream))){
            while(bufferedReader.ready()){
                text.append((char)bufferedReader.read());
--- src/main/java/com/javarush/cryptoanalyzer/bulanov/servise/FileCrypto.java
+++ src/main/java/com/javarush/cryptoanalyzer/bulanov/servise/FileCrypto.java
@@ -9,18 +9,19 @@ public class FileCrypto{
     public char[] fileEncryption(){
 
         StringBuilder cryptoText = new StringBuilder();
-        FileRead originalText = new FileRead();
-        for(int i = 0; i < originalText.fileRead().length(); i++){
 
-            int offset = RandomOffset.randomOffset();
-            int index = CryptoAlphabets.ALPHABET.indexOf(originalText.fileRead().charAt(i));
+        StringBuilder originalText = new FileRead().fileRead();
+        int offset = RandomOffset.randomOffset();
+        for(int i = 0; i < originalText.length(); i++){
+
+            int index = CryptoAlphabets.ALPHABET.indexOf(originalText.charAt(i));
 
             if(index == -1){
-                cryptoText.append(originalText.fileRead().charAt(i));
+                cryptoText.append(originalText.charAt(i));
 
             }else if((index + offset) >= CryptoAlphabets.ALPHABET.length()){
-                offset = offset - (CryptoAlphabets.ALPHABET.length() - 1 - index);
-                cryptoText.append(CryptoAlphabets.ALPHABET.charAt(offset -1));
+                int offsetTemp = offset - (CryptoAlphabets.ALPHABET.length() - 1 - index);
+                cryptoText.append(CryptoAlphabets.ALPHABET.charAt(offsetTemp -1));
             }else{
                 cryptoText.append(CryptoAlphabets.ALPHABET.charAt(index + offset));
             }
